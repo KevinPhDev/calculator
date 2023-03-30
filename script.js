ec7482f063f325e8f7fa61fc79e6097c2e545508
KevinPhDev <kevinphdev@outlook.com>
2023-03-30 18:24:02 +1100

    Declare operand and operator variables

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
+let num1;
+let num2;
+let operator;
 
 
 
-function add(a, b) {
-    return a + b;
+function add(num1, num2) {
+    return num1 + num2;
 }
 
-function subtract(a, b) {
-    return a - b;
+function subtract(num1, num2) {
+    return num1 - num2;
 }
 
-function multiply(a, b) {
-    return a * b;
+function multiply(num1, num2) {
+    return num1 * num2;
 }
 
-function divid(a, b) {
-    return a / b;
+function divide(num1, num2) {
+    return num1 / num2;
 }
+
+
