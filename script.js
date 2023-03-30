dc38f0cfc27a76f771057662496f2bba51f3ec9a
KevinPhDev <kevinphdev@outlook.com>
2023-03-30 18:25:30 +1100

    Create an operate function to call any 4 operator functions

--- script.js
+++ script.js
@@ -21,3 +21,9 @@ function divide(num1, num2) {
 }
 
 
+function operate(num1, num2, operator) {
+    if (operator == '+') return add(num1, num2);
+    if (operator == '-') return subtract(num1, num2);
+    if (operator == '*') return multiply(num1, num2);
+    if (operator == '/') return divide(num1, num2);
+}
