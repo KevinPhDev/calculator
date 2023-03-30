b7c6abc44924609bb95401c6a8958bbcad68cef4
KevinPhDev <kevinphdev@outlook.com>
2023-03-30 15:37:19 +1100

    Create functions for basic operations

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divid(a, b) {
+    return a / b;
+}
